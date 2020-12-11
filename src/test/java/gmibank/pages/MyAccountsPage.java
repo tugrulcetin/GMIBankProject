74fcef16d893768c3e509ad04ff77cfd57b9a5c5
tugrulcetin <av.tugrulcetin@gmail.com>
2020-12-11 01:17:43 +0100

    MyAccountsPageFromTugrul

--- src/test/java/gmibank/pages/MyAccountsPage.java
+++ src/test/java/gmibank/pages/MyAccountsPage.java
@@ -1,2 +1,21 @@
-package gmibank.pages;public class MyAccountsPage {
+package gmibank.pages;
+
+import gmibank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MyAccountsPage {
+
+    public MyAccountsPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy (xpath = "//*[@id=\"entity-menu\"]/div/a[1]")
+    public WebElement MyAccounts;
+
+    @FindBy (xpath = "//*[@id=\"app-view-container\"]/div[1]/div/div/div/h2")
+    public WebElement customerAccountsText;
+
+
 }
